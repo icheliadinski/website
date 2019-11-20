9a3c01dfb51785ad0b0626babfa692ff570800bb
Igor Cheliadinski <igorexec94@gmail.com>
2019-11-19 23:30:37 -0500

    init commit

---
+++ src/@app/__tests__/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow, ShallowWrapper } from 'enzyme'
+import { App } from '../App'
+
+describe('[App]', () => {
+  let wrapper: ShallowWrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('renders successfully', () => {
+    wrapper.toMatchSnapshot()
+  })
+})
