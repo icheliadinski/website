3f90b9f79727e1031f72f60b62f78ec8c2b2f2ac
Igor Cheliadinski <icheliadinski@gmail.com>
2019-11-20 21:43:42 -0500

    new: dev: Add stylelint

---
+++ src/@pages/under-construction/UnderConstruction.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './UnderConstruction.module.scss'
+
+export const UnderConstruction = () => {
+  return <div className={styles.card}>Under Construction</div>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Root } from '@app'
 
 import * as serviceWorker from './serviceWorker'
-import './index.css'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 
