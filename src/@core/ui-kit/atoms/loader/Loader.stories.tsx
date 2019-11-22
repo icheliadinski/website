c048a5e36845cb47b2ee99eae83008d88ac76e30
Igor Cheliadinski <icheliadinski@gmail.com>
2019-11-22 13:55:59 -0500

    new: add states for storybook

--- src/@core/ui-kit/atoms/loader/Loader.stories.tsx
+++ src/@core/ui-kit/atoms/loader/Loader.stories.tsx
@@ -3,4 +3,6 @@ import { Loader } from "@core/ui-kit";
 
 export default { title: "Loader", component: Loader };
 
-export const empty = () => <Loader />;
+export const initial = () => <Loader />;
+export const notCentered = () => <Loader isCentered={false} />;
+export const withCustomCaption = () => <Loader>Caption</Loader>;
