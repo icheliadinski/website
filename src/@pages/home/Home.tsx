ace98cf21bcce870f483ddaec466c703769706d5
Igor Cheliadinski <igorexec94@gmail.com>
2019-11-23 15:05:39 -0500

    new: add animation for menu item selection

--- src/@pages/home/Home.tsx
+++ src/@pages/home/Home.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { LeftMenu, ICON_TYPES } from '@core/ui-kit'
 
 export const Home = () => {
-  const [activeMenuItem, setActiveMenuItem] = useState<ICON_TYPES | null>(null)
+  const [activeMenuItem, setActiveMenuItem] = useState<ICON_TYPES>(
+    ICON_TYPES.USER_ICON,
+  )
   const profileIcons = [
     ICON_TYPES.USER_ICON,
     ICON_TYPES.PHONE_ICON,
@@ -10,8 +12,7 @@ export const Home = () => {
     ICON_TYPES.MONITOR_ICON,
     ICON_TYPES.GLASSES_ICON,
   ]
-  const handleLeftMenuClick = (item: ICON_TYPES | null) =>
-    setActiveMenuItem(item)
+  const handleLeftMenuClick = (item: ICON_TYPES) => setActiveMenuItem(item)
 
   return (
     <main>
