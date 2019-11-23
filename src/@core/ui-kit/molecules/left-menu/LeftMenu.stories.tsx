60ada8106682a6b508ac70878ac086330d5dee21
Igor Cheliadinski <igorexec94@gmail.com>
2019-11-23 00:06:14 -0500

    new: add left menu storybook

--- src/@core/ui-kit/molecules/left-menu/LeftMenu.stories.tsx
+++ src/@core/ui-kit/molecules/left-menu/LeftMenu.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LeftMenu } from './LeftMenu'
 
+// tslint:disable-next-line no-default-export
 export default { title: 'Left Menu', component: LeftMenu }
 
 export const initial = () => <LeftMenu />
