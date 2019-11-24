f4f35ff5cf4272ad4d093f95090ea93e1ccf6da8
Igor Cheliadinski <igorexec94@gmail.com>
2019-11-24 09:44:34 -0500

    new: add storybook for about and sidecard

---
+++ src/@core/public-components/about/About.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { About } from './About'
+
+// tslint:disable-next-line no-default-export
+export default { title: 'About', component: About }
+
+export const Default = () => <About />
---
+++ src/@core/ui-kit/atoms/side-card/SideCard.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { SideCard } from './SideCard'
+
+// tslint:disable-next-line no-default-export
+export default { title: 'Side Card', component: SideCard }
+
+export const Default = () => <SideCard>Content</SideCard>
