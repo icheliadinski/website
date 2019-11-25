bb1e568aa2f7e43b652a780b72ec76d758f6626c
Igor Cheliadinski <igorexec94@gmail.com>
2019-11-24 22:11:42 -0500

    chg: add new icons

--- src/@core/ui-kit/atoms/icon/Icon.stories.tsx
+++ src/@core/ui-kit/atoms/icon/Icon.stories.tsx
@@ -5,9 +5,9 @@ import { ICON_TYPES } from './constants'
 // tslint:disable-next-line no-default-export
 export default { title: 'Icon', component: Icon }
 
-export const Phone = () => <Icon type={ICON_TYPES.PHONE_ICON} />
-export const Glasses = () => <Icon type={ICON_TYPES.GLASSES_ICON} />
-export const User = () => <Icon type={ICON_TYPES.USER_ICON} />
-export const Monitor = () => <Icon type={ICON_TYPES.MONITOR_ICON} />
-export const Edit = () => <Icon type={ICON_TYPES.EDIT_ICON} />
-export const Download = () => <Icon type={ICON_TYPES.DOWNLOAD_ICON} />
+export const Phone = () => <Icon type={ICON_TYPES.PHONE} />
+export const Glasses = () => <Icon type={ICON_TYPES.GLASSES} />
+export const User = () => <Icon type={ICON_TYPES.USER} />
+export const Monitor = () => <Icon type={ICON_TYPES.MONITOR} />
+export const Edit = () => <Icon type={ICON_TYPES.EDIT} />
+export const Download = () => <Icon type={ICON_TYPES.DOWNLOAD} />
--- src/@core/ui-kit/molecules/left-menu/ProfileMenu.tsx
+++ src/@core/ui-kit/molecules/left-menu/ProfileMenu.tsx
@@ -4,7 +4,7 @@ import { Icon, ICON_TYPES } from '@core/ui-kit'
 import styles from './LeftMenu.module.scss'
 
 export interface Props {
-  navigationItems?: Array<{ path: string; icon: ICON_TYPES; title: string }>
+  navigationItems?: Array<{ path: string; icon: ICON_TYPES; title?: string }>
 }
 
 export const ProfileMenu: React.FC<Props> = ({ navigationItems = [] }) => {
