d642a9688ad5f9ef55c0973ae726f02a0be2b249
Igor Cheliadinski <igorexec94@gmail.com>
2020-01-04 22:57:26 -0500

    chg: remove storybook

--- src/@core/ui-kit/atoms/loader/Loader.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Loader } from './Loader'
-
-// tslint:disable-next-line no-default-export
-export default { title: 'Loader', component: Loader }
-
-export const initial = () => <Loader />
-export const withCustomCaption = () => <Loader>Caption</Loader>
--- src/@core/ui-kit/atoms/side-card/SideCard.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { SideCard } from './SideCard'
-
-// tslint:disable-next-line no-default-export
-export default { title: 'Side Card', component: SideCard }
-
-export const Default = () => <SideCard>Content</SideCard>
