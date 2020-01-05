2eb3d9ab0915b700b15f6949d7a0c6701adc9bce
Igor Cheliadinski <icheliadinski@gmail.com>
2020-01-04 22:57:26 -0500

    chg: remove storybook

--- src/@core/public-components/about/About.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { About } from './About'
-
-// tslint:disable-next-line no-default-export
-export default { title: 'About', component: About }
-
-export const Default = () => <About />
